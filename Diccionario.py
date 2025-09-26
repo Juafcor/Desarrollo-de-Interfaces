fa42c6feb528f94acc07cc73c7096baaca9bde04
JUAN FRANCISCO RIVAS RODRIGO <jrivarod@myuax.com>
2025-09-26 10:10:56 +0200

    tupla

--- Diccionario.py
+++ Diccionario.py
@@ -1,5 +1,10 @@
 dicc={'28493354P': 'persona1', '37482749R': 'persona2', '12345678P': 'persona3'}
 
-for i in dicc:
+for dni,nombre in dicc.items():
 
-    print(i)
+    print(f"datos persona:", dni, nombre)
+
+
+tupla = (1,2,3)
+
+tupla2=(tupla,4)
